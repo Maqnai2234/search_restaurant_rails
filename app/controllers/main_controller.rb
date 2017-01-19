985ee908465c7de3f04c4fc36e67e01059722d28
Alexei <alexeim763@gmail.com>
2017-01-19 00:42:34 -0500

    add functionality for city

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,23 @@
 class MainController < ApplicationController
+  before_action :check_city_selected, only: [:landing]
   def landing
     @cities = City.all
+
+  end
+
+  def city_selected
+    if params[:city].nil?
+      if session[:city].nil?
+        redirect_to root_path
+      end
+    else
+      session[:city] = params[:city]
+    end
+  end
+
+  def check_city_selected
+    unless session[:city].nil?
+      redirect_to city_selected_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "main#landing"
+  post "/city_selected", to: "main#city_selected"
+  get "/city_selected", to: "main#city_selected"
 end
