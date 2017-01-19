1c80ac2849becee42db652300d96b82cb08fa21d
Alexei <alexeim763@gmail.com>
2017-01-19 10:37:07 -0500

    Add admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   post "/app", to: "main#app"
   get "/app", to: "main#app"
   delete "/remove_city", to: "main#remove_city"
+
+  namespace :admin do
+    get '/dashboard', to: "dashboard#index"
+  end
+
+  get '/admin' => redirect('/admin/dashboard')
 end
